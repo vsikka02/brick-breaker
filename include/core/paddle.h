4e8839a06ee82cc7d6ff2054549b53263ab87d9f
vanshsikka <vsikka2@illinois.edu>
2021-04-20 14:40:19 -0500

    Added Paddle and Paddle Collision Code that allows the User to Interact with the Ball.

---
+++ include/core/paddle.h
@@ -0,0 +1,33 @@
+//
+// Created by Vansh Sikka on 4/18/21.
+//
+
+#pragma once
+
+#include <glm/fwd.hpp>
+#include <glm/vec2.hpp>
+
+
+namespace brickbreaker {
+
+class Paddle {
+ public:
+  int const kPaddleRadius = 50;
+  int const kStartingPointOfPaddle = 375;
+
+  enum Direction {
+    MOVE_LEFT,
+    MOVE_RIGHT,
+  };
+
+  Paddle();
+  void Draw() const;
+  void KeyPress(enum Direction direction, bool boolean);
+  glm::vec2& position();
+
+ private:
+  glm::vec2 position_;
+};
+
+}
+
