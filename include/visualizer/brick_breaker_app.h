6ecc81531f2b4fbe38db66d71b9329fcc498af28
vanshsikka <vsikka2@illinois.edu>
2021-04-20 14:38:54 -0500

    Generated Basic Files needed to start Cinder Project.

--- include/visualizer/brick_breaker_app.h
+++ include/visualizer/brick_breaker_app.h
@@ -4,9 +4,13 @@
 
 #pragma once
 
+#include <core/paddle.h>
+
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "game_environment.h"
+#include "visualizer/game_environment.h"
 
 namespace brickbreaker{
 
@@ -14,10 +18,19 @@ namespace visualizer {
 
 class BrickBreakerApp : public ci::app::App {
  public:
-  BrickBreakerApp();
 
-  const double kWindowSize = 800;
+  BrickBreakerApp();
+  void draw() override;
+  void keyDown(ci::app::KeyEvent event) override;
+  void keyUp(ci::app::KeyEvent event) override;
+  void update() override;
+
+
+  const double kWindowSize = 750;
   const double kMargin = 100;
+
+ private:
+  GameEnvironment game_environment_;
 };
 
 }
