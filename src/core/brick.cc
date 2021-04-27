51ed8f873797fb5eac7629a1bd228a583b3cf86c
vanshsikka <vsikka2@illinois.edu>
2021-04-27 17:12:44 -0500

    Made Minor Changes to Brick object.

--- src/core/brick.cc
+++ src/core/brick.cc
@@ -8,7 +8,7 @@
 
 namespace brickbreaker {
 
-Brick::Brick(glm::vec2& position, size_t life_span, size_t width, size_t height) {
+Brick::Brick(glm::vec2& position, int life_span, int width, int height) {
   position_ = position;
   life_span_ = life_span;
   width_ = width;
@@ -16,13 +16,19 @@ Brick::Brick(glm::vec2& position, size_t life_span, size_t width, size_t height)
 }
 
 void Brick::Draw() const {
-  if (life_span_ > 0) {
+  if (!(life_span_ == 0)) {
     glm::vec2 upper_bound =
         glm::vec2(position_.x + width_, position_.y + height_);
     glm::vec2 lower_bound =
         glm::vec2(position_.x - width_, position_.y - height_);
 
-    ci::gl::color(ci::Color("red"));
+    if (life_span_ == 3) {
+      ci::gl::color(ci::Color("red"));
+    } else if (life_span_ == 2) {
+      ci::gl::color(ci::Color("blue"));
+    } else if (life_span_ == 1) {
+      ci::gl::color(ci::Color("green"));
+    }
     ci::gl::drawSolidRect(ci::Rectf(lower_bound, upper_bound));
   }
 }
@@ -37,10 +43,46 @@ std::pair<glm::vec2, glm::vec2> Brick::BrickBoundaries() {
 }
 
 void Brick::Update() {
-  life_span_-= 1;
+  if (!(life_span_ == 0)) {
+    life_span_ -= 1;
+  }
+  if (life_span_ == 0) {
+    position_ = glm::vec2(0,0);
+    width_ = 0;
+    height_ = 0;
+  }
 }
 
-size_t Brick::life_span() {
+int Brick::life_span() const{
   return life_span_;
+}
+
+glm::vec2 Brick::position() const{
+  return position_;
+}
+
+int Brick::height() const{
+  return height_;
+}
+
+void Brick::life_span(int life_span) {
+  life_span_ = life_span;
+}
+
+int Brick::width() const {
+  return width_;
+}
+
+void Brick::width(int width) {
+  width_ = width;
+}
+
+void Brick::height(int height) {
+  height_ = height;
+}
+
+void Brick::position(float x, float y) {
+  position_.x = x;
+  position_.y = y;
 }
 }
