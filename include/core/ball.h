e964e63b7d1756db59432ea4e3485e98c0cb8eea
vanshsikka <vsikka2@illinois.edu>
2021-04-27 17:12:14 -0500

    Ball Handles Wall Collisions and Paddle Collisions properly with angles.

--- include/core/ball.h
+++ include/core/ball.h
@@ -14,28 +14,28 @@
 namespace brickbreaker {
 class Ball {
  public:
-  Ball(){};
+  Ball() {};
 
-  static size_t const kBallRadius = 10;
+  static constexpr float kBallRadius = 10;
 
-  Ball(const glm::vec2& position, const glm::vec2& velocity, const size_t radius_ = kBallRadius);
+  Ball(const glm::vec2& position, const glm::vec2& velocity, const float radius_ = kBallRadius);
 
   void Draw() const;
 
   glm::vec2 position() const;
   glm::vec2 velocity() const;
-  size_t radius() const;
+  float radius() const;
 
   void LaunchBall();
   void UpdatePosition(brickbreaker::Paddle& paddle);
   void WallCollision(glm::vec2& in_bounds_1, glm::vec2& in_bounds_2);
   void PaddleCollision(brickbreaker::Paddle& paddle);
-  void BrickCollision(brickbreaker::Brick& brick);
+  void BrickCollision(std::vector<brickbreaker::Brick>& bricks);
 
  private:
   glm::vec2 position_;
   glm::vec2 velocity_;
-  size_t radius_;
-  bool is_launched = false;
+  float radius_;
+  bool is_launched;
 };
 }
