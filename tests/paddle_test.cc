b4a278d8dc9844fdea8aba4252267c087c1eea1d
vanshsikka <vsikka2@illinois.edu>
2021-04-27 17:15:16 -0500

    Implemented Basic Testing For Paddle.

--- tests/paddle_test.cc
+++ tests/paddle_test.cc
@@ -7,7 +7,24 @@
 #include <catch2/catch.hpp>
 #include <glm/common.hpp>
 
-TEST_CASE("Position Getter") {
+using glm::vec2;
+
+TEST_CASE("Paddle Movement") {
   brickbreaker::Paddle paddle = brickbreaker::Paddle();
-  paddle.position();
+  SECTION("Paddle Move Left") {
+    paddle.KeyPress(brickbreaker::Paddle::MOVE_LEFT, true);
+    REQUIRE(paddle.position() == vec2(365, 680));
+  }
+  SECTION("Paddle Move Right") {
+    paddle.KeyPress(brickbreaker::Paddle::MOVE_RIGHT, true);
+    REQUIRE(paddle.position() == vec2(385, 680));
+  }
+  SECTION("Paddle Move Right with No Key Pressed") {
+    paddle.KeyPress(brickbreaker::Paddle::MOVE_RIGHT, false);
+    REQUIRE(paddle.position() == vec2(375, 680));
+  }
+  SECTION("Paddle Move Left with No Key Pressed") {
+    paddle.KeyPress(brickbreaker::Paddle::MOVE_LEFT, false);
+    REQUIRE(paddle.position() == vec2(375, 680));
+  }
 }
