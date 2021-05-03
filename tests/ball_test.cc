7b1e441e716faad16803ed1af8b717bc6240388a
vanshsikka <vsikka2@illinois.edu>
2021-05-03 16:40:35 -0500

    Implemented Test Cases and Added new Tests!

--- tests/ball_test.cc
+++ tests/ball_test.cc
@@ -9,35 +9,35 @@ TEST_CASE("Ball Position Test") {
   brickbreaker::Paddle paddle;
   SECTION("Ball Move Down") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(150, 150), vec2(0, -1), 10);
-    ball.LaunchBall();
+    ball.Launch();
     ball.UpdatePosition(paddle);
     REQUIRE(ball.position() == vec2(150, 149));
   }
 
   SECTION("Ball Move") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(150, 150), vec2(1, 1), 10);
-    ball.LaunchBall();
+    ball.Launch();
     ball.UpdatePosition(paddle);
     REQUIRE(ball.position() == vec2(151, 151));
   }
 
   SECTION("Ball Move Up") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(150, 150), vec2(0, 1), 10);
-    ball.LaunchBall();
+    ball.Launch();
     ball.UpdatePosition(paddle);
     REQUIRE(ball.position() == vec2(150, 151));
   }
 
   SECTION("Ball Move Left") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(150, 150), vec2(-1, 0), 10);
-    ball.LaunchBall();
+    ball.Launch();
     ball.UpdatePosition(paddle);
     REQUIRE(ball.position() == vec2(149, 150));
   }
 
   SECTION("Ball Move Right") {
     brickbreaker::Ball ball = brickbreaker::Ball (vec2(150, 150), vec2(1, 0), 10);
-    ball.LaunchBall();
+    ball.Launch();
     ball.UpdatePosition(paddle);
     REQUIRE(ball.position() == vec2(151, 150));
   }
@@ -51,7 +51,7 @@ TEST_CASE("Ball Position Test") {
   SECTION("Ball Not Launched Paddle Moved") {
     brickbreaker::Ball ball = brickbreaker::Ball (vec2(150, 150), vec2(1, 0), 10);
     for (int i = 0; i < 2; i++) {
-      paddle.KeyPress(brickbreaker::Paddle::MOVE_LEFT, true);
+      paddle.KeyPress(brickbreaker::Paddle::LEFT, true);
       ball.UpdatePosition(paddle);
     }
     REQUIRE(ball.position() == vec2(paddle.position().x, paddle.position().y - ball.radius()));
@@ -67,7 +67,7 @@ TEST_CASE("Ball Wall Collision Velocity Test") {
     REQUIRE(ball.velocity() == vec2(1, 1));
   }
 
-  SECTION("Particle Check Left Wall Collision") {
+  SECTION("Ball Check Left Wall Collision") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(161, 172), vec2(1, -1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
@@ -75,7 +75,7 @@ TEST_CASE("Ball Wall Collision Velocity Test") {
     REQUIRE(ball.velocity() == vec2(-1, -1));
   }
 
-  SECTION("Particle Check Right Wall Collision") {
+  SECTION("Ball Check Right Wall Collision") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(189, 172), vec2(1, 1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
@@ -83,15 +83,15 @@ TEST_CASE("Ball Wall Collision Velocity Test") {
     REQUIRE(ball.velocity() == vec2(-1, 1));
   }
 
-  SECTION("Particle Check Bottom Dead Wall Collision") {
-    brickbreaker::Ball ball = brickbreaker::Ball(vec2(172, 189), vec2(-1, 1), 1);
+  SECTION("Ball Check Bottom Dead Wall Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(172, 191), vec2(-1, 1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
     ball.WallCollision(in_bound_1, in_bound_2);
     REQUIRE(ball.velocity() == vec2(0, 0));
   }
 
-  SECTION("Particle Check Top-Left Corner Collision") {
+  SECTION("Ball Check Top-Left Corner Collision") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(161, 161), vec2(-1, -1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
@@ -99,7 +99,7 @@ TEST_CASE("Ball Wall Collision Velocity Test") {
     REQUIRE(ball.velocity() == vec2(1, 1));
   }
 
-  SECTION("Particle Check Top-Right Corner Collision") {
+  SECTION("Ball Check Top-Right Corner Collision") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(189, 161), vec2(1, -1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
@@ -107,16 +107,16 @@ TEST_CASE("Ball Wall Collision Velocity Test") {
     REQUIRE(ball.velocity() == vec2(-1, 1));
   }
 
-  SECTION("Particle Check Dead Bottom-Left Corner Collision") {
-    brickbreaker::Ball ball = brickbreaker::Ball(vec2(161, 189), vec2(-1, 1), 1);
+  SECTION("Ball Check Dead Bottom-Left Corner Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(161, 191), vec2(-1, 1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
     ball.WallCollision(in_bound_1, in_bound_2);
     REQUIRE(ball.velocity() == vec2(0, 0));
   }
 
-  SECTION("Particle Check Dead Bottom-Right Corner Collision") {
-    brickbreaker::Ball ball = brickbreaker::Ball(vec2(189, 189), vec2(1, 1), 1);
+  SECTION("Ball Check Dead Bottom-Right Corner Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(189, 191), vec2(1, 1), 1);
     vec2 in_bound_1 = vec2(160, 160);
     vec2 in_bound_2 = vec2(190, 190);
     ball.WallCollision(in_bound_1, in_bound_2);
@@ -130,25 +130,29 @@ TEST_CASE("Ball Brick Collision Velocity Test") {
   brickbreaker::Brick brick = brickbreaker::Brick(brick_position, 3, 50, 30);
   bricks.push_back(brick);
   SECTION("Ball Collision With Right Side of Brick") {
-    brickbreaker::Ball ball = brickbreaker::Ball(vec2(209, 220), vec2(-1, 1), 1);
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(209, 210), vec2(-1, 1), 1);
     ball.BrickCollision(bricks);
-    REQUIRE(ball.velocity() == vec2(1, -1));
+    REQUIRE(ball.velocity() == glm::vec2(1,1));
   }
+
   SECTION("Ball with No Brick Collisions") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(400, 400), vec2(-1, 1), 1);
     ball.BrickCollision(bricks);
     REQUIRE(ball.velocity() == vec2(-1, 1));
   }
+
   SECTION("Ball Collision With Left Side of Brick") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(109, 220), vec2(1, -1), 1);
     ball.BrickCollision(bricks);
     REQUIRE(ball.velocity() == vec2(-1, -1));
   }
+
   SECTION("Ball Collision with Top of Brick") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(120, 161), vec2(1, 1), 1);
     ball.BrickCollision(bricks);
     REQUIRE(ball.velocity() == vec2(1, -1));
   }
+
   SECTION ("Ball Collision with Bottom of Brick") {
     brickbreaker::Ball ball = brickbreaker::Ball(vec2(120, 221), vec2(-1, -1), 1);
     ball.BrickCollision(bricks);
@@ -157,5 +161,52 @@ TEST_CASE("Ball Brick Collision Velocity Test") {
 }
 
 TEST_CASE("Ball and Paddle Collision") {
+  brickbreaker::Paddle paddle;
+  paddle.set_position(375,680);
+  SECTION("Middle of Paddle Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(375, 680), vec2(-1, 1), 1);
+    ball.set_is_launched(true);
+    ball.PaddleCollision(paddle);
+    REQUIRE(ball.velocity() == vec2(-1, -1));
+  }
 
+  SECTION("Right of Paddle Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(395, 680), vec2(-1, 1), 1);
+    ball.set_is_launched(true);
+    vec2 old_velocity = ball.velocity();
+    ball.PaddleCollision(paddle);
+    vec2 new_velocity = old_velocity - (2.0f * glm::dot(vec2(sin(-0.314159), cos(-0.314159)), old_velocity) * vec2(sin(-0.314159), cos(-0.314159)));
+    REQUIRE(ball.velocity().x == Approx(new_velocity.x));
+    REQUIRE(ball.velocity().y == Approx(new_velocity.y));
+  }
+
+  SECTION("Left of Paddle Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(355, 680), vec2(-1, 1), 1);
+    ball.set_is_launched(true);
+    vec2 old_velocity = ball.velocity();
+    ball.PaddleCollision(paddle);
+    vec2 new_velocity = old_velocity - (2.0f * glm::dot(vec2(sin(0.314159), cos(0.314159)), old_velocity) * vec2(sin(0.314159), cos(0.314159)));
+    REQUIRE(ball.velocity().x == Approx(new_velocity.x));
+    REQUIRE(ball.velocity().y == Approx(new_velocity.y));
+  }
+
+  SECTION("Left Corner Paddle Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(325, 680), vec2(-1, 1), 1);
+    ball.set_is_launched(true);
+    vec2 old_velocity = ball.velocity();
+    ball.PaddleCollision(paddle);
+    vec2 new_velocity = old_velocity - (2.0f * glm::dot(vec2(sin(0.785398), cos(0.785398)), old_velocity) * vec2(sin(0.785398), cos(0.785398)));
+    REQUIRE(ball.velocity().x == Approx(new_velocity.x));
+    REQUIRE(ball.velocity().y == -Approx(new_velocity.y));
+  }
+
+  SECTION("Right Corner Paddle Collision") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(425, 680), vec2(-1, 1), 1);
+    ball.set_is_launched(true);
+    vec2 old_velocity = ball.velocity();
+    ball.PaddleCollision(paddle);
+    vec2 new_velocity = old_velocity - (2.0f * glm::dot(vec2(sin(-0.785398), cos(-0.785398)), old_velocity) * vec2(sin(-0.785398), cos(-0.785398)));
+    REQUIRE(ball.velocity().x == Approx(new_velocity.x));
+    REQUIRE(ball.velocity().y == Approx(new_velocity.y));
+  }
 }
--- tests/brick_test.cc
+++ tests/brick_test.cc
@@ -2,6 +2,7 @@
 // Created by Vansh Sikka on 4/27/21.
 //
 
+#include <core/ball.h>
 #include <core/brick.h>
 
 #include <catch2/catch.hpp>
@@ -14,4 +15,40 @@ TEST_CASE("Brick Boundary") {
   brickbreaker::Brick brick = brickbreaker::Brick(brick_position, 3, 30, 50);
   REQUIRE(brick.BrickBoundaries().first == vec2(70, 150));
   REQUIRE(brick.BrickBoundaries().second == vec2(130, 250));
+}
+
+TEST_CASE("Brick Collision") {
+  std::vector<brickbreaker::Brick> bricks;
+  glm::vec2 brick_position = vec2(159, 191);
+  brickbreaker::Brick brick = brickbreaker::Brick(brick_position, 3, 50, 30);
+  bricks.push_back(brick);
+  SECTION("Ball Collision With Right Side of Brick") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(209, 210), vec2(-1, 1), 1);
+    ball.BrickCollision(bricks);
+    REQUIRE(bricks[0].life_span() == 2);
+  }
+
+  SECTION("Ball with No Brick Collisions") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(400, 400), vec2(-1, 1), 1);
+    ball.BrickCollision(bricks);
+    REQUIRE(bricks[0].life_span() == 3);
+  }
+
+  SECTION("Ball Collision With Left Side of Brick") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(109, 220), vec2(1, -1), 1);
+    ball.BrickCollision(bricks);
+    REQUIRE(bricks[0].life_span() == 2);
+  }
+
+  SECTION("Ball Collision with Top of Brick") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(120, 161), vec2(1, 1), 1);
+    ball.BrickCollision(bricks);
+    REQUIRE(bricks[0].life_span() == 2);
+  }
+
+  SECTION ("Ball Collision with Bottom of Brick") {
+    brickbreaker::Ball ball = brickbreaker::Ball(vec2(120, 221), vec2(-1, -1), 1);
+    ball.BrickCollision(bricks);
+    REQUIRE(bricks[0].life_span() == 2);
+  }
 }
